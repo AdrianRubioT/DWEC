51ef439a83725d5f4e36864e0b9693c1945f65f2
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-09-20 20:30:35 +0200

    intentado entender

---
+++ Ejercicio002/scrip.js
@@ -0,0 +1,12 @@
+/* Ejercicio 2
+Según Wikipedia, un número feliz se define mediante el siguiente proceso:
+
+"Comenzando con cualquier número entero positivo, 
+reemplace el número por la suma de los cuadrados de
+sus dígitos, y repita el proceso hasta que el número sea igual a 1 (donde permanecerá), o se repite
+infinitamente en un ciclo que no incluye 1.
+Aquellos números para los que este proceso termina en 1 son números felices, mientras que aquellos que no
+terminan en 1 son números infelices (o números tristes) ".
+Escribe un programa de JavaScript para encontrar e imprimir los primeros 5 números felices. */
+
+
