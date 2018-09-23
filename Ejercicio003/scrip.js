7f55ee27240c84e2b39672ada8670b81ccf0c81b
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-09-23 18:16:09 +0200

    acavado

--- Ejercicio003/scrip.js
+++ Ejercicio003/scrip.js
@@ -10,13 +10,22 @@ una URL válida (incluyendo el protocolo),
 Ten en cuenta que en el DNI, no solo tiene que comprobar si el formato es correcto, si no que también
 tiene que comprobar si la letra concuerda con la serie numérica. */
 
+pedirCorreo();
+pedirDNI();
+pedirIP();
+pedirTlf();
+pedirURL();
+
+
+
+
 
 function pedirDNI() {
 
     var dni = prompt("Introduzca el DNI");
 
     /* https://donnierock.com/2011/11/05/validar-un-dni-con-javascript/ */
-    var expresion_regular_dni
+    var expresion_regular_dni;
 
     expresion_regular_dni = /^\d{8}[a-zA-Z]$/;
 
@@ -40,16 +49,57 @@ function pedirDNI() {
 
 function pedirCorreo() {
 
+    var correo = prompt("Introduzca un correo electronico");
+
+    var expresion_regular_correo;
+    expresion_regular_correo = /^[_a-z0-9-]+(.[_a-z0-9-]+)*@[a-z0-9-]+(.[a-z0-9-]+)*(.[a-z]{2,4})$/;
+
+   if (expresion_regular_correo.test(correo)){
+        alert("Es correcto");
+    }else{
+        alert("No es correcto");
+    }
+
 }
 
 function pedirTlf() {
 
+    var tlf = prompt("Introduzca un telefono");
+
+    var expresion_regular_tlf = /^((\+?34([ \t|\-])?)?[9|6|7]((\d{1}([ \t|\-])?[0-9]{3})|(\d{2}([ \t|\-])?[0-9]{2}))([ \t|\-])?[0-9]{2}([ \t|\-])?[0-9]{2})$/;
+
+   if (expresion_regular_tlf.test(tlf)){
+        alert("Es correcto");
+    }else{
+        alert("No es correcto");
+    }
+
 }
 
-function pedirip() {
+function pedirIP() {
 
+    var ip = prompt("Introduzca una IP");
+
+    var expresion_regular_ip = /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/;
+
+   if (expresion_regular_ip.test(ip)){
+        alert("Es correcto");
+    }else{
+        alert("No es correcto");
+    }
 }
 
 function pedirURL() {
+    
+
+    var url = prompt("Introduzca una URL");
+
+    var expresion_regular_URL = /(http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/;
+
+   if (expresion_regular_URL.test(url)){
+        alert("Es correcto");
+    }else{
+        alert("No es correcto");
+    }
 
 }
