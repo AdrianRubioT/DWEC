522d1fb071a655c21d4f697bdb800746bb13e578
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-09-23 18:35:26 +0200

    trabajando

---
+++ Ejercicio004/scrip.js
@@ -0,0 +1,16 @@
+
+/* Ejercicio 4
+Escribir una función a la que le pasemos un argumento 
+y nos indique si este es un objeto Date o no.
+ */
+
+function esDate(arg){
+
+    if (arg.getdate()) {
+        alert("Es una clase date");
+    }else{
+        alert("No es una clase date");
+    }
+
+}
+
---
+++ Ejercicio005/scrip.js
@@ -0,0 +1,17 @@
+
+
+/* Ejercicio 5
+Escribir una función que devuelva el 
+número de días que tiene un mes indicado. 
+Le pasaremos como argumento el 
+número de mes y el número de año.
+
+document.write(obtenerDiasMes(1, 2012);
+Resultado: 31
+
+ */
+
+function obtenerDiasMes(mes, anio){
+
+}
+
