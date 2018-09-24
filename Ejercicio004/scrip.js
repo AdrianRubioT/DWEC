ebb6d9d9a6ab4f19b3908c9a5345cedce9726188
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-09-24 15:44:16 +0200

    cambiar la comprovacion a Date.parse

--- Ejercicio004/scrip.js
+++ Ejercicio004/scrip.js
@@ -4,9 +4,15 @@ Escribir una función a la que le pasemos un argumento
 y nos indique si este es un objeto Date o no.
  */
 
+
+var fecha = "5sa/6/2019";
+
+esDate(fecha);
+
 function esDate(arg){
 
-    if (arg.getdate()) {
+
+    if (Date.parse(arg)) {
         alert("Es una clase date");
     }else{
         alert("No es una clase date");
