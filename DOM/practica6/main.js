722b910f9f44515ac74d30292d35b0aaea7801ae
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-11-15 19:19:04 +0100

    Mucho curro

--- DOM/practica6/main.js
+++ DOM/practica6/main.js
@@ -1,48 +1,79 @@
 
+var segundos = 1;
+var timer;
+var videoActivo = "video1";
+
+/* variable para avanzar o retroceder el timepo del */
+var tiempo = 10;
+
+var video = document.getElementsByClassName("reproductor")[0].children;
+
+
 function cargar() {
 
-    document.getElementById("anuncio").addEventListener("click", anuncio, false);
+    timer = setInterval(correrTiempo, 1000);
+    document.getElementById("playPausa").addEventListener("click", playPauseVideo, false);
+    document.getElementById("rebobinar").addEventListener("click", retrocederVideo, false);
+    document.getElementById("avanzar").addEventListener("click", avanzarVideo, false);
+
+
+}
+
+
+function avanzarVideo() {
+
+
     
-
 }
 
-function cerrarAnuncio() {
-
-    document.getElementById("anuncio").className = oculto;
-
+function retrocederVideo() {
+    
 }
 
-function anuncio() {
-    alert("anuncio");
-    var segundos = 10;
-    var timer = setInterval(correrTiempo(), 1000);
 
-    function setTiempo() {
-        segundos = 10;
-        document.getElementById("cuentaAtras").removeEventListener("click", cerrarAnuncio, false);
+function playPauseVideo() {
+    
+    var boton = document.getElementById("playPausa");
+
+
+    if (boton.className != "play"){
+        boton.className = "play";
+        /* document.getElementById(videoActivo).play(); */
+        video.play();
+
+    }else{
+        boton.className = "pause";
+        /* document.getElementById(videoActivo).pause(); */
+        video.pause();
     }
+}
 
-    function correrTiempo() {
 
-        if (segundos == 0) {
 
-            clearInterval(timer);
-            
-            document.getElementById("cuentaAtras").addEventListener("click", cerrarAnuncio, false);
 
-            actualizarTiempo()
-        }
-    }
-
-    function actualizarTiempo() {
-
-    alert("hola mundo");
-        document.getElementById("cuentaAtras").Value=--segundos;
-    }
-
-    setTiempo();
-    correrTiempo();
+function ocultarAnuncio() {
+    document.getElementById("anuncio").className = "oculto";
 
 }
+function correrTiempo() {
+    if (segundos == 0) {
+        
+        clearInterval(timer);
+        var aspa = document.getElementById("aspa");
+        aspa.addEventListener("click", ocultarAnuncio, false);
+        aspa.style.cursor = "pointer";
+
+        segundos = 1;
+
+    } else {
+
+        document.getElementById("cuentaAtras").innerText=--segundos;
+        /* console.log(segundos); */
+    }
+}
+
+
+
+
 
 document.addEventListener("DOMContentLoaded", cargar, false);
