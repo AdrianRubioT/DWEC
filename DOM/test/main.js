5114f909d7af9fefc0f397d577c1eef1b96b59b9
AdrianRubioT <adrianalumnosmx@gmail.com>
2018-11-19 20:06:16 +0100

    text

---
+++ DOM/test/main.js
@@ -0,0 +1,10 @@
+
+function cargar() {
+    document.getElementById("boton").addEventListener("click", text, false);
+}
+
+function text(event) {
+    console.log(event);
+}
+
+document.addEventListener("DOMContentLoaded", cargar, false);
